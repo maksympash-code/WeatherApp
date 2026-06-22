f06c351b5cb80fe1594c602968a730cc17d3054d
maksympash <maksympash@gmail.com>
2026-06-22 23:19:43 +0300

    Initial project setup

---
+++ app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package ua.knu.maksym_pashchenko.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ua.knu.maksym_pashchenko.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WeatherApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherApp() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "WeatherApp \uD83C\uDF26\uFE0F"
+        )
+    }
+}
