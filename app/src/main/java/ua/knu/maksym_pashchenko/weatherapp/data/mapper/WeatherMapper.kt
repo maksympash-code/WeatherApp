96f2ba7a93ab7a80b44aa961f5dc66884280131f
maksympash <maksympash@gmail.com>
2026-06-23 17:42:59 +0300

    Fix weather DTO mapper

--- app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/mapper/WeatherMapper.kt
+++ app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/mapper/WeatherMapper.kt
@@ -3,15 +3,15 @@ package ua.knu.maksym_pashchenko.weatherapp.data.mapper
 import ua.knu.maksym_pashchenko.weatherapp.data.remote.dto.WeatherDto
 import ua.knu.maksym_pashchenko.weatherapp.domain.model.Weather
 
-fun WeatherDto.toDomainWeather(): Weather {
+fun WeatherDto.toDomain(): Weather {
     val weatherDescription = weather.firstOrNull()
 
     return Weather(
         cityName = name,
         temperature = main.temp,
         description = weatherDescription?.description ?: "No description",
-        iconUrl = weatherDescription?.icon.let {
-            "https://openweathermap.org/img/wn/\${iconCode}@2x.png"
+        iconUrl = weatherDescription?.icon.let { iconCode ->
+            "https://openweathermap.org/img/wn/${iconCode}@2x.png"
         },
         humidity = main.humidity,
         windSpeed = wind.speed,
