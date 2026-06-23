714b33171655c177b68d02a017b8e7cb90c0e665
maksympash <maksympash@gmail.com>
2026-06-23 17:39:23 +0300

    Add weather DTO to domain mapper

---
+++ app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/mapper/WeatherMapper.kt
@@ -0,0 +1,20 @@
+package ua.knu.maksym_pashchenko.weatherapp.data.mapper
+
+import ua.knu.maksym_pashchenko.weatherapp.data.remote.dto.WeatherDto
+import ua.knu.maksym_pashchenko.weatherapp.domain.model.Weather
+
+fun WeatherDto.toDomainWeather(): Weather {
+    val weatherDescription = weather.firstOrNull()
+
+    return Weather(
+        cityName = name,
+        temperature = main.temp,
+        description = weatherDescription?.description ?: "No description",
+        iconUrl = weatherDescription?.icon.let {
+            "https://openweathermap.org/img/wn/\${iconCode}@2x.png"
+        },
+        humidity = main.humidity,
+        windSpeed = wind.speed,
+        updatedAt = ""
+    )
+}
