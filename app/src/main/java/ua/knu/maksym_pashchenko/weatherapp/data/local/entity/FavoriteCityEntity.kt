127503462526a4a147353c9712aa7897ff7ce086
maksympash <maksympash@gmail.com>
2026-07-22 21:14:03 +0300

    Fix favorite cities table name

--- app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/local/entity/FavoriteCityEntity.kt
+++ app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/local/entity/FavoriteCityEntity.kt
@@ -3,7 +3,7 @@ package ua.knu.maksym_pashchenko.weatherapp.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "favourite_cities")
+@Entity(tableName = "favorite_cities")
 data class FavoriteCityEntity(
     @PrimaryKey
     val cityName: String,
