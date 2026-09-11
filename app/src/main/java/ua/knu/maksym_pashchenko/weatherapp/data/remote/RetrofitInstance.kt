eaa718a38e61fa31a53c0ee7e05e2461856c5551
maksympash <maksympash@gmail.com>
2026-09-11 23:41:19 +0300

    Final WeatherApp review

--- app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/remote/RetrofitInstance.kt
+++ app/src/main/java/ua/knu/maksym_pashchenko/weatherapp/data/remote/RetrofitInstance.kt
@@ -4,13 +4,18 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ua.knu.maksym_pashchenko.weatherapp.BuildConfig
 import ua.knu.maksym_pashchenko.weatherapp.data.remote.api.WeatherApiService
 
 object RetrofitInstance {
     private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
     }
 
     private val okHttpClient = OkHttpClient.Builder()
